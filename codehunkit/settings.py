50f5ea442958b55a90e46a8986cf94418bab78a8
Faraz Masood Khan <faraz@fanaticlab.com>
2013-11-21 20:58:40 +0500

    basic css added, also integrating django-pipeline for compress and css compiler

--- codehunkit/settings.py
+++ codehunkit/settings.py
@@ -121,6 +121,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'codehunkit.app',
+    'pipeline',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
