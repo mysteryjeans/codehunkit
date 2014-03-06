541961f334117fddd09790efce5c070e5bc6ab59
Faraz Masood Khan <mk.faraz@gmail.com>
2014-03-06 16:17:43 +0500

    changes...

--- codehunkit/app/context_processors.py
+++ codehunkit/app/context_processors.py
@@ -10,10 +10,11 @@ def bootstrip(request):
     """
     Setting basic context variables
     """
-    
+    default_langs = Language.get_defaults()
     return {
             'app_user': request.user,
             'hunkies': request.user.get_followings() if request.user.is_authenticated() else None,            
             'now': datetime.datetime.now(),
-            'langs': request.user.get_langs() if request.user.is_authenticated() else Language.get_defaults(),
+            'langs': request.user.get_langs() if request.user.is_authenticated() else default_langs,
+            'default_langs': default_langs
     }
