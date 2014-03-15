af4a1803e7dce33a556a8a53ff3c118a272f1b8e
Faraz Masood Khan <faraz@fanaticlab.com>
2014-03-15 17:57:21 +0500

    Logo and icon updated

--- codehunkit/settings.py
+++ codehunkit/settings.py
@@ -9,6 +9,8 @@ DEV_MACS = ('Dev-Mac', 'Envy15', 'envy', 'trg-tech-farazm', 'faraz-VirtualBox', 
 DEBUG = socket.gethostname() in DEV_MACS
 TEMPLATE_DEBUG = DEBUG
 
+BASE_DIR = os.path.dirname(__file__)
+
 ADMINS = (
     # ('Faraz Masood Khan', 'faraz@codehunkit.com'),
 )
@@ -56,7 +58,7 @@ USE_TZ = False
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = '' if DEBUG else '/home/jeans/www/static/codehunkit/'
+MEDIA_ROOT = '' if DEBUG else '/home/jeans/www/media/codehunkit/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -67,7 +69,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = '' if DEBUG else '/home/jeans/www/static/codehunkit/'
+STATIC_ROOT = BASE_DIR if DEBUG else '/home/jeans/www/static/codehunkit/'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
