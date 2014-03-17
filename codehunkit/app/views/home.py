04ea7f3d6f652241db411b6f5d6cdcbf1dfd5cce
Faraz Masood Khan <faraz@fanaticlab.com>
2014-03-17 14:24:04 +0500

    Working on top/new snippets feature

--- codehunkit/app/views/home.py
+++ codehunkit/app/views/home.py
@@ -3,9 +3,15 @@
 @copyright: Copyright (c) 2013 FanaticLab
 """
 
+import urllib
+
 from django.conf import settings
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+
 from codehunkit.app.views import render_response
-from codehunkit.app.models import Snippet
+from codehunkit.app.models import Snippet, User, Language
+
 
 
 def index(request, page_index=0, sort_by_new=False):
@@ -28,3 +34,30 @@ def search(request, page_index=0, sort_by_new=False):
     """
     Display snippets by user, language or search term
     """
+    q = request.REQUEST.get('q', None)
+    if q:
+        q = q.strip()
+        
+        users_q = User.objects.filter(username__iexact=q)[:1]
+        if users_q:            
+            return HttpResponseRedirect(reverse('app_user', args=[users_q[0].username]))
+        
+        lang_q = Language.objects.filter(name__iexact=q)[:1]
+        if lang_q:            
+            return HttpResponseRedirect(reverse('app_lang', args=[lang_q[0].name]))
+        
+        params = { 'q': q.encode('utf-8')}
+        query = '?' + urllib.urlencode(params)
+        source = {'title': q,
+                  'absolute_url': reverse('app_search') + query,
+                  'absolute_url_by_new': reverse('app_search_new') + query,
+                  'active': 'new' if sort_by_new else 'top'}
+        #posts = Post.tag_posts(q, int(page_index), settings.PAGE_SIZE, sort_by_new, request.app_user)
+                
+        #top_tags = Tag.top_tags()
+        #top_channels = Channel.top_channels()                
+        #prev_url, next_url = paginated_url(request.resolver_match.url_name, posts, [page_index], params)
+                
+        #return render_response(request, 'app/search.html', locals())
+    
+    return HttpResponseRedirect(reverse('app_home'))
