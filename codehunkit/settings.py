83cdf61f0eff8f01e670a563c44327255e6c03b7
Faraz Masood Khan <faraz@fanaticlab.com>
2014-03-18 08:25:43 +0500

    Server deployment restructured

--- codehunkit/settings.py
+++ codehunkit/settings.py
@@ -71,7 +71,7 @@ USE_TZ = False
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = '' if DEBUG else '/home/jeans/www/media/codehunkit/'
+MEDIA_ROOT = '' if DEBUG else '/home/jeans/www/codehunkit.com/media/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -82,7 +82,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = BASE_DIR if DEBUG else '/home/jeans/www/static/codehunkit/'
+STATIC_ROOT = BASE_DIR if DEBUG else '/home/jeans/www/codehunkit.com/static/'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
