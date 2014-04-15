ca36c15f78abc0c98a4a1cee77ee2ce953560439
Faraz Masood Khan <faraz@fanaticlab.com>
2014-04-15 13:03:01 +0500

    Auto fill updated_by and created_by columns in Django Admin on save

--- codehunkit/app/admin.py
+++ codehunkit/app/admin.py
@@ -8,8 +8,23 @@ Django administration for Codehunkit
 from codehunkit.app import models
 from django.contrib import admin
 
+class ModelAdmin(admin.ModelAdmin):
+    """
+    Abstract admin models for populating columns updated_by and created_by
+    """
+    exclude = ('updated_by', 'created_by',)
+    def save_form(self, request, form, change):
+        obj = super(ModelAdmin, self).save_form(request, form, change)
+        
+        if hasattr(obj, 'updated_by'):
+            obj.updated_by = unicode(request.user)
+       
+        if hasattr(obj, 'created_by') and not obj.created_by:
+            obj.created_by = unicode(request.user)
+            
+        return obj    
 
-class LanguageAdmin(admin.ModelAdmin):
+class LanguageAdmin(ModelAdmin):
     list_display = ('name', 'website', 'description',)
     prepopulated_fields = {'slug': ('name',)}
     list_filter = ('created_on',)
@@ -17,45 +32,45 @@ class LanguageAdmin(admin.ModelAdmin):
     date_hierarchy = 'created_on'
 
 
-class LanguageGraphAdmin(admin.ModelAdmin):
+class LanguageGraphAdmin(ModelAdmin):
     list_display = ('language', 'up_votes', 'down_votes', 'readers_count', 'coders_count', 'snippets_count')
     list_filter = ('updated_on',)
     search_fields = ('language',)
     date_hierarchy = 'updated_on'
 
     
-class LanguageBadgeSummaryAdmin(admin.ModelAdmin):
+class LanguageBadgeSummaryAdmin(ModelAdmin):
     list_display = ('language', 'badge', 'awarded_count',)
 
 
-class BadgeAdmin(admin.ModelAdmin):
+class BadgeAdmin(ModelAdmin):
     list_display = ('name', 'group_name', 'description',)    
 
 
-class SchoolAdmin(admin.ModelAdmin):
+class SchoolAdmin(ModelAdmin):
     list_display = ('name',)
     prepopulated_fields = {'slug': ('name',)}
 
 
-class SchoolGraphAdmin(admin.ModelAdmin):
+class SchoolGraphAdmin(ModelAdmin):
     list_display = ('school', 'up_votes', 'down_votes',  'coders_count',)
 
 
-class SchoolBadgeSummaryAdmin(admin.ModelAdmin):
+class SchoolBadgeSummaryAdmin(ModelAdmin):
     list_display = ('school', 'badge', 'awarded_count',)
     
 
-class UserAdmin(admin.ModelAdmin):
+class UserAdmin(ModelAdmin):
     list_display = ('username', 'email', 'gender',)
     list_filter = ('gender', 'created_on')
 
 
-class SnippetAdmin(admin.ModelAdmin):
+class SnippetAdmin(ModelAdmin):
     list_display = ('id', 'gist', 'user', 'language', 'votes', 'rank', 'comments_count', 'created_on',)
     date_hierarchy = 'created_on'
 
 
-class CommentAdmin(admin.ModelAdmin):
+class CommentAdmin(ModelAdmin):
     list_display = ('id', 'user', 'comment_text', 'votes', 'created_on',)
     date_hierarchy = 'created_on'
 
